3b1011b7b660966a98a3e8091caa55a884272d4e
Ruslan Liashkevich <rliashkevich@gmail.com>
2025-08-02 20:28:25 +0200

    Delete player.py

--- player.py
+++
@@ -1,9 +0,0 @@
-class Player:
-    def __init__(self, name, age):
-        self.name = name
-        self.age = age
-        self.party = []
-        
-
-    def __str__(self):
-        return "I'm {} and I'm {} years old".format(self.name, self.age)
